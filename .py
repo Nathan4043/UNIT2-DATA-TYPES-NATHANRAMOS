99eb9e8d67d73bc6887904226e5039d9bb39517e
Nathan4043 <nathanr88@nycstudents.net>
2025-09-15 07:17:38 -0700

    d
    d

---
+++ .py
@@ -0,0 +1,25 @@
+# x = 3
+# y = float(3)
+# print(x,y)
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(i)
+# print(values[0])
+# print(values[6])
+#this works
+# what_is_the_weather = input("whats the weather")
+# if what_is_the_weather == " sunny":
+#     print("correct")
+# else:
+#     print("false")
+# x = "test"
+# print(f"hello {x}")
+
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
